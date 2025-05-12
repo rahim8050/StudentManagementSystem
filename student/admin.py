701fe79abe584aaf1f6ee64e913cf3f63928367b
rahim8050 <rahimranxx8050@gmail.com>
2025-05-12 14:21:30 +0100

    models

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from student.models import Student
+
+from student.models import Student
+admin.site.site_header = 'Student Administration'
+admin.site.site_title = 'Student Administration'
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'FirstName', 'LastName', 'Gender')
+    list_filter = ('Gender',)
+    list_per_page = 15
--- student/models.py
+++ student/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    Email = models.EmailField(unique=True)
+    Age = models.PositiveSmallIntegerField(default=0)
+    Gender = models.CharField(max_length=10)
+    Weight = models.PositiveIntegerField(default=0)
+    Semester = models.PositiveSmallIntegerField(default=0)
+    StudentID = models.PositiveIntegerField()
+    AdmissionNumber = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
