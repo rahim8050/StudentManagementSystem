65d3993b769998a461b091e808e77fa34698f40e
rahim8050 <rahimranxx8050@gmail.com>
2025-05-12 14:33:11 +0100

    models

--- student/admin.py
+++ student/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from student.models import Student
 
 from student.models import Student
+admin.site.register(Student)
 admin.site.site_header = 'Student Administration'
 admin.site.site_title = 'Student Administration'
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('id', 'FirstName', 'LastName', 'Gender')
-    list_filter = ('Gender',)
+    list_filter = ('Gender')
     list_per_page = 15
